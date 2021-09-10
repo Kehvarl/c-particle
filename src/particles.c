4f3e3746d6fb4bcc3501ab1f496d7fef6c2faf2a
Kehvarl <kehvarl@kehvarl.com>
2021-09-09 19:36:48 -0700

    Setup project ideas

--- src/particles.c
+++ src/particles.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
+struct simulation {
+    int width;
+    int height;
+    char world[];
+};
+
 int main(void)
 {
-    puts("Hello, World");
+    /* Read first line from STDIN
+       Looking for INT INT \n
+       providing width, height*/
+
+    /* Initialize simulation cells*/
+    struct simulation sim;
+
+
+    /* Read HEIGHT lines from STDIN with WIDTH chars per cell 
+       Set initial simulation state */
+    
+    /*Tick the simulation state
+      If the Tick returns no falling stones, the simulation is complete
+      Otherwise repeat*/
+
+    return 0
+}
+
+int tick(struct simulation sim)
+{
     return 0;
 }
