0250f85e4fd5d8d0101108dd66b84c5658cd01eb
Kehvarl <kehvarl@kehvarl.com>
2021-09-10 23:05:20 -0700

    Really inefficient

--- src/particles.c
+++ src/particles.c
@@ -2,13 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_ROCKS 2;
+
 struct simulation {
     int width;
     int height;
-    char* world;
+    int* world;
 };
 
 int tick(struct simulation *sim);
+int print_world(struct simulation *sim);
 
 int main(void)
 {
@@ -37,10 +40,32 @@ int main(void)
     /* Initialize simulation cells*/
     sim.width = width;
     sim.height = height;
-    sim.world = malloc(sizeof(char) * (width * height + 1));
+    sim.world = malloc(sizeof(int) * (width * height + 1));
 
     /* Read HEIGHT lines from STDIN with WIDTH chars per cell 
-       Set initial simulation state */
+       Set initial simulation state 
+       
+       Cell Contents Representation:
+       * "T": -1
+       * " ":  0
+       * ".":  1
+       * ":":  2
+    */
+
+   for(int r = 0; r < sim.height; l++){
+       getline(&line, &size, stdin);
+       for(int c = 0; c < sim.width; c++){
+           if(strcmp(line[c], " ") == 0){
+               sim.world[r*c] = 0;
+           }else if(strcmp(line[c], ".") == 0){
+               sim.world[r*c] = 1;
+           }else if(strcmp(line[c], ":") == 0){
+               sim.world[r*c] = 2;
+           }else if(strcmp(line[c], "T") == 0){
+               sim.world[r*c] = -1;
+           }
+       }
+   }
     
 
     /* Tick the simulation state
@@ -50,13 +75,49 @@ int main(void)
         tick_results = tick(&sim);
     } while(tick_results > 0);
 
+    print_world(&sim);
+
     /* Call free on anything malloced */
     free(sim.world);
-    
+
     return 0;
 }
 
 int tick(struct simulation *sim)
 {
+    char *temp = malloc(sizeof(int) * (sim->width * sim->height + 1));
+
+    for(int row=0; row < sim->height; row++){
+        for(int col=0; col < sim->width; col++){
+            /* Current cell: sim->world[r*c]
+               Target cell:  sim->world[r*c+sim->width]
+               If Current Cell is 0 or -1 or Target Cell is -1: skip
+               else: Move rocks from Current to Target 
+                     until Target is at MAX or Current is at 0
+               */
+        }
+    }
+
+    free(temp);
+    return 0;
+}
+
+int print_world(struct simulation *sim)
+{
+    for(int row=0; row < sim->height; row++){
+        for(int col=0; col < sim->width; col++){
+            if(sim->world[row*col] == -1){
+                printf("T");
+            } else if(sim->world[row*col] == 0){
+                printf(" ");
+            } else if(sim->world[row*col] == 1){
+                printf(".");
+            } else if(sim->world[row*col] == 2){
+                printf(":");
+            }
+
+        }
+        printf("\n");
+    }
     return 0;
 }
